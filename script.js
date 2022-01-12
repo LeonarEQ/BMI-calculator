7cea18ac86f75131e0f75ae8bbd559137f80fdcb
LeonarEQ <leonardestupinan@yahoo.com>
2022-01-12 14:05:29 +0100

    Ejercicio Resuelto

--- script.js
+++ script.js
@@ -9,5 +9,24 @@ form.addEventListener('submit', function (e) {
 
   // Tu código aquí --->
 
+   
+  // Asignar los valores de los inputs a variables
+  let height = form.querySelector('#height').value;
+  let weight = form.querySelector('#weight').value;
+  let imc;
+  
+  // Condición si inputs vacíos 
+
+  if (!height || !weight) {   
+
+    // Mostrar "Faltan Valores" en pantalla.
+    form.querySelector('#results').innerText = "Faltan Valores";
+      
+    } else { 
+    // Calculo del imc
+    imc = weight / ((height * height) / 10000);
+
+    // Mostrar resultado en pantalla.    
+    form.querySelector('#results').innerText = Math.round(imc *10) /10;}
 
 });
